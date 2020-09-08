a65f17f55a7fec3a5cdf703925e2eecebf153525
Mike <sharvi77777@gmail.com>
2020-09-08 18:50:02 +1000

    Initial Commit

---
+++ ShapovalovAV_HW2.7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShapovalovAV_HW2.7
+//
+//  Created by Arthur on 08.09.2020.
+//  Copyright © 2020 Arthur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
